22f33e06a1125bf7f3d4501b79b9acc8e3996381
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-01-06 09:21:09 +0530

    vercel commit

--- frontend/src/Components/pages/SignUp.jsx
+++ frontend/src/Components/pages/SignUp.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [firstName,setfname]=useState("");
+    const [lastName,setlname]=useState("");
+    const [email,setemail]=useState("");
+    const [password,setpassword]=useState("");
+
+    var signin =()=>{
+        var req=axios.post("https://localhost:3001/signup",{
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            password:password
+        })
+        console.log(req)
+    }
+
+
   return (
     <div>
         <h2>SignIn</h2>
+        <form action="">
         <label htmlFor="firstname">First Name  </label>
         <input type="text"/><br />
         <label htmlFor="lastname">Last Name</label>
@@ -13,6 +31,7 @@ const SignUp = () => {
         <label htmlFor="password">Password</label>
         <input type="text"/><br />
         <button  type='submit'>SignIn</button>
+        </form>
       
     </div>
   )
